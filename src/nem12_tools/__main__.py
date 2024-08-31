390f785df5be05a192de2b3f36200f54ee38cf2e
Josh Smeaton <josh.smeaton@gmail.com>
2024-08-31 20:53:13 +1000

    Remove redundant output and entrypoint

--- src/nem12_tools/__main__.py
+++
@@ -1,4 +0,0 @@
-from nem12_tools.cli import generate
-
-if __name__ == "__main__":
-    generate()
--- src/nem12_tools/cli.py
+++ src/nem12_tools/cli.py
@@ -11,7 +11,6 @@ from nem12_tools.parsers.nmid import from_nmidiscovery
 @click.argument("output_file", type=click.File("wb"))
 def generate(nmi_discovery_file: IO[str], output_file: IO[str]) -> None:
     meter_config = from_nmidiscovery(nmi_discovery_file.read())
-    click.echo(meter_config)
     meter_data_transaction = generate_nem12(meter_config)
     meter_data_transaction.tree.write(
         output_file, pretty_print=True, xml_declaration=True, encoding="utf-8"
